4e78a3b1bff3f416a6b9f6cc13e6aacbf8b05789
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-01-05 13:08:55 -0500

    fixed pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,12 +6,12 @@
 
 def minOperations(n):
     """Returns the minimum number of operations to reach a number"""
-    
+
     if type(n) is not int or n <= 1:
         return 0
     nOperations = 0
     half = 2
-    while n > 1:
+    while (n > 1):
         if n % half == 0:
             nOperations += half
             n /= half
