244b8262b88c82149975e0ec4f8ea27a10cf881d
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-01-05 13:03:48 -0500

    ADDED files

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Minimum Operations to be performed
+    Resurses: https://www.youtube.com/watch?v=03ZepmoVJGI
+"""
+
+
+def minOperations(n):
+    """Returns the minimum number of operations to reach a number"""
+    
+    if type(n) is not int or n <= 1:
+        return 0
+    nOperations = 0
+    half = 2
+    while (n > 1):
+        if n % half == 0:
+            nOperations += half
+            n /= half
+        else:
+            half += 1
+    return nOperations
